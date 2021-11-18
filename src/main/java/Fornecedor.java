18d3b6bbc38e73f5bebb3ec1bd9c01ffb695cdd7
Laura <lauraizabel8569@gmail.com>
2021-11-18 20:16:45 -0300

    criacao das classes de Fornecedor, auditoria, produto e reserva

---
+++ src/main/java/Fornecedor.java
@@ -0,0 +1,57 @@
+public class Fornecedor {
+    private Number id;
+    private String nome;
+    private String cnpj;
+    private String endereco;
+    private String telefone;
+    private String email;
+
+    public Fornecedor(Number id, String nome, String cnpj, String endereco, String telefone, String email) {
+        this.id = id;
+        this.nome = nome;
+        this.cnpj = cnpj;
+        this.endereco = endereco;
+        this.telefone = telefone;
+        this.email = email;
+    }
+
+    public Number getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
