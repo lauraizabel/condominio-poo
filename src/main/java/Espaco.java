b2dcb61aa86986e3e2561d517d4f6e4cfdbbcece
gloriadasilva <gloria.silva@picpay.com>
2021-11-18 09:11:44 -0300

    criacao das classes Carro, Espaco e Funcionario. Remocao dos setId das outras classes.

---
+++ src/main/java/Espaco.java
@@ -0,0 +1,41 @@
+public class Espaco {
+    String id;
+    String nome;
+    int capacidade;
+    boolean ocupado;
+
+    public Espaco(String id, String nome, int capacidade, boolean ocupado) {
+        this.id = id;
+        this.nome = nome;
+        this.capacidade = capacidade;
+        this.ocupado = ocupado;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getCapacidade() {
+        return capacidade;
+    }
+
+    public void setCapacidade(int capacidade) {
+        this.capacidade = capacidade;
+    }
+
+    public boolean isOcupado() {
+        return ocupado;
+    }
+
+    public void setOcupado(boolean ocupado) {
+        this.ocupado = ocupado;
+    }
+}
---
+++ src/main/java/Funcionario.java
@@ -0,0 +1,63 @@
+import java.util.Date;
+
+public class Funcionario {
+    String id;
+    String cargo;
+    Double salario;
+    String endereco;
+    Date dataAdmissao;
+    Date dataEmissao;
+
+    public Funcionario(String id, String cargo, Double salario, String endereco, Date dataAdmissao, Date dataEmissao){
+        this.id = id;
+        this.cargo = cargo;
+        this.salario = salario;
+        this.endereco = endereco;
+        this.dataAdmissao = dataAdmissao;
+        this.dataEmissao = dataEmissao;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public Double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(Double salario) {
+        this.salario = salario;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public Date getDataAdmissao() {
+        return dataAdmissao;
+    }
+
+    public void setDataAdmissao(Date dataAdmissao) {
+        this.dataAdmissao = dataAdmissao;
+    }
+
+    public Date getDataEmissao() {
+        return dataEmissao;
+    }
+
+    public void setDataEmissao(Date dataEmissao) {
+        this.dataEmissao = dataEmissao;
+    }
+}
