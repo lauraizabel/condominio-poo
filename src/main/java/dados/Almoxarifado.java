6d5f8a09f71875b6b88cadf3499268e017f428ae
Eduartthy Felipe <73499401+eduartthy@users.noreply.github.com>
2021-11-26 09:56:05 -0300

    Update Almoxarifado.java

--- src/main/java/dados/Almoxarifado.java
+++ src/main/java/dados/Almoxarifado.java
@@ -3,7 +3,13 @@ package dados;
 import java.util.ArrayList;
 
 public class Almoxarifado {
-    private ArrayList<Produto> produtos = new ArrayList<>();
+    private String id;
+    private ArrayList<Produto> produtos = new ArrayList<Produto>();
+    private ArrayList<Servico> servicos = new ArrayList<Servico>();
+
+    public String getId() {
+        return this.id;
+    }
 
     public Almoxarifado(ArrayList<Produto> produtos){
         this.produtos = produtos;
@@ -15,5 +21,13 @@ public class Almoxarifado {
 
     public void setProdutos(ArrayList<Produto> produtos) {
         this.produtos = produtos;
+    }
+
+    public ArrayList<Servico> getServicos() {
+        return this.servicos;
+    }
+
+    public void setServicos(ArrayList<Servico> servicos) {
+        this.servicos = servicos;
     }
 }
