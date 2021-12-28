3b460a22aae9865d84039e6b296b1c7cb1d234c1
Eduartthy Felipe <73499401+eduartthy@users.noreply.github.com>
2021-12-28 01:49:34 -0300

    Estilizacao
    Versão de testes de estilização

--- src/main/java/application/MainApplication.java
+++ src/main/java/application/MainApplication.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
@@ -11,9 +12,13 @@ public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("main-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 900, 600);
+
+        //remove a borda - para usar precisa ter um meio de mudar a posição dela e colocar botões de ação, como fechar em outro lugar
+        //stage.initStyle(StageStyle.UNDECORATED);
+
         stage.setTitle("Condomínio POO");
-        stage.setMaximized(true);
+        //stage.setMaximized(true);
         stage.setScene(scene);
         stage.show();
     }
